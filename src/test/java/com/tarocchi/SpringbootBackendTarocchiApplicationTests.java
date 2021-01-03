4c58bba4b59589efb26795763cacc3b5df6e3794
Adina <adina.informatica@gmail.com>
2021-01-03 16:40:07 +0100

    first commit

---
+++ src/test/java/com/tarocchi/SpringbootBackendTarocchiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarocchi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBackendTarocchiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
